7c5a2beb11837a58e9cd86ef614748e9c1ee0858
vankovanko <118365437+vankovanko@users.noreply.github.com>
2022-11-21 09:57:51 +0200

    Add files via upload

---
+++ Zad3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+int osn1;
+int osn2;
+int vis;
+printf("Vavedete osn1:");
+scanf("%d",&osn1);
+printf("Vavedete osn2:");
+scanf("%d",&osn2);
+printf("Vavedete vis:");
+scanf("%d",&vis);
+printf("Liceto e:%d\n", (osn1+osn2)*vis/2);
+
+return 0;
+}
